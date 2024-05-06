240ad638f0609eb6916663a210f5c11861b11fd6
mithun30052001 <lmithun01@gmail.com>
2024-05-06 15:33:31 +0530

    normal radio is modified as mat-radio buttons

--- src/app/below-menu/below-menu.component.ts
+++ src/app/below-menu/below-menu.component.ts
@@ -8,19 +8,31 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 
 export class BelowMenuComponent {
+  locations:any = [];
+  selectedLocation: string = '';
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.locations = [
+      { id: 'chennai', value: 'Chennai' },
+      { id: 'coimbatore', value: 'Coimbatore' },
+      { id: 'hyderabad', value: 'Hyderabad' },
+      { id: 'lucknow', value: 'Lucknow' },
+      { id: 'jaipur', value: 'Jaipur' },
+      { id: 'bengaluru', value: 'Bengaluru' },
+      { id: 'vellore', value: 'Vellore' },
+      { id: 'ahmedabad', value: 'Ahmedabad' },
+      { id: 'us', value: 'US' },
+      { id: 'manila', value: 'Manila' }
+    ];
     this.route.queryParams.subscribe(params => {
-      const location = params['location'];
-      if (location) {
-        (document.getElementById(location.toLowerCase()) as HTMLInputElement).checked = true;
-      }
+      this.selectedLocation = params['location'];
     });
   }
 
   updateLocation(event: any) {
-    const location = event.target.value;
+    console.log("Event",event);
+    const location = event.value;
     this.router.navigate(['/jobs'], { queryParams: { location: location } });
   }
 }
