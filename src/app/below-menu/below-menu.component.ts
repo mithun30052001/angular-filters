2f4864c069c76d4748bdec37744a899010343325
mithun30052001 <lmithun01@gmail.com>
2024-05-06 12:58:58 +0530

    method for adding location queryparams and a logic to make the location selected if location param is present on init is done

--- src/app/below-menu/below-menu.component.ts
+++ src/app/below-menu/below-menu.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-below-menu',
   templateUrl: './below-menu.component.html',
   styleUrls: ['./below-menu.component.scss']
 })
-export class BelowMenuComponent {
 
+export class BelowMenuComponent {
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      const location = params['location'];
+      if (location) {
+        (document.getElementById(location.toLowerCase()) as HTMLInputElement).checked = true;
+      }
+    });
+  }
+
+  updateLocation(event: any) {
+    const location = event.target.value;
+    this.router.navigate(['/jobs'], { queryParams: { location: location } });
+  }
 }
