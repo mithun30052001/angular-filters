81e26bdd018b7fea4ed7820c24e01cb1be55a402
mithun30052001 <lmithun01@gmail.com>
2024-05-07 11:33:48 +0530

    timings component for adding timings in the params is done

---
+++ src/app/timings/timings.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GenericSelectionComponent } from '../helpers/generic-selection.component';
+
+@Component({
+  selector: 'app-timings',
+  templateUrl: './timings.component.html',
+  styleUrls: ['./timings.component.scss']
+})
+export class TimingsComponent extends GenericSelectionComponent{
+  timings:any = [];
+  selectedTiming: string = '';
+  constructor(router: Router, route: ActivatedRoute) { 
+    super(router, route);
+  }
+
+  ngOnInit(): void {
+    this.timings = [
+      { id: 'night', value: 'Night shift' },
+      { id: 'day', value: 'Day shift' }
+    ];
+    this.route.queryParams.subscribe(params => {
+      this.selectedTiming = params['timings'];
+    });
+  }
+
+  updateTiming(event: any) {
+    super.updateOption(event, 'timings');
+  }
+}
