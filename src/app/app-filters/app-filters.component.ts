f07b9d3f74a141dfff449786880e80fa1f37a1cb
mithun30052001 <lmithun01@gmail.com>
2024-05-08 12:43:27 +0530

    A generic filter component for filter

---
+++ src/app/app-filters/app-filters.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GenericSelectionComponent } from '../helpers/generic-selection';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './app-filters.component.html',
+  styleUrls: ['./app-filters.component.scss']
+})
+
+export class AppFiltersComponent extends GenericSelectionComponent{
+  @Input() choices:any = []
+  @Input() paramKey:string = '';
+  @Input() header:string = '';
+  selectedChoice: string = '';
+  constructor(router: Router,route: ActivatedRoute) {
+    super(router, route);
+   }
+
+  ngOnInit(): void {
+    this.selectedChoice = this.setSelectedOption(this.paramKey);
+  }
+
+  updateFilter(event: any) {
+    super.updateOption(event,this.paramKey);
+  }
+  
+  resetFilter(paramKey: string) {
+    const queryParams = { ...this.route.snapshot.queryParams };
+    delete queryParams[paramKey];
+    this.router.navigate([], { queryParams: queryParams });
+    this.selectedChoice = '';
+  }
+}
