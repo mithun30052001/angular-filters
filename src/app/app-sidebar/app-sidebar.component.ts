593e7224ed7b180bf4d834994d81c73d34919027
mithun30052001 <lmithun01@gmail.com>
2024-05-08 12:43:04 +0530

    location and timings called as a generic filter component by passing in the reuired data

--- src/app/app-sidebar/app-sidebar.component.ts
+++ src/app/app-sidebar/app-sidebar.component.ts
@@ -8,10 +8,28 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 })
 export class AppSidebarComponent {
   isJobsRoute: boolean = false;
-
+  locations:any = [];
+  timings:any = [];
   constructor(private router: Router) {}
 
   ngOnInit() {
+    this.locations = [
+      { id: 'chennai', value: 'Chennai' },
+      { id: 'coimbatore', value: 'Coimbatore' },
+      { id: 'hyderabad', value: 'Hyderabad' },
+      { id: 'lucknow', value: 'Lucknow' },
+      { id: 'jaipur', value: 'Jaipur' },
+      { id: 'bengaluru', value: 'Bengaluru' },
+      { id: 'vellore', value: 'Vellore' },
+      { id: 'ahmedabad', value: 'Ahmedabad' },
+      { id: 'us', value: 'US' },
+      { id: 'manila', value: 'Manila' }
+    ];
+    this.timings = [
+      { id: 'night', value: 'Night shift' },
+      { id: 'day', value: 'Day shift' },
+      { id: 'notGiven', value: 'Not given' }
+    ];
     this.router.events.subscribe(() => {
         this.isJobsRoute = this.router.url.includes('/jobs');
     });
