a2f11c22c2f8dffa898888f07925ce008fdde1b8
mithun30052001 <lmithun01@gmail.com>
2024-05-10 13:24:01 +0530

    params fetched and listings updated for filterconsumer

--- src/app/filter-consumer-container/filter-consumer-container.component.ts
+++ src/app/filter-consumer-container/filter-consumer-container.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { JobsService } from '../services/job.services';
+import { GenericSelectionComponent } from '../models/generic-selection';
 
 @Component({
   selector: 'filter-consumer-container',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./filter-consumer-container.component.scss']
 })
 export class FilterConsumerContainerComponent {
+  @Input() filteredListings: any[] = [];
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  itemsPerPage: number = 5;
+  private queryParamsSubscription!: Subscription;
 
+  constructor(
+    private jobsService: JobsService,
+    private genericSelection: GenericSelectionComponent
+  ) {}
+
+  ngOnInit(): void {
+    this.queryParamsSubscription =
+      this.genericSelection.allQueryParams$.subscribe((params) => {
+        console.log('Inside referral listing in generic', params);
+        this.filteredListings = this.jobsService.getJobListings(params);
+        if ('itemsPerPage' in params) {
+          this.itemsPerPage = parseInt(params['itemsPerPage']as string);
+        }
+        this.filteredListings = this.filteredListings.slice(0,this.itemsPerPage);
+        console.log("List in referral",this.filteredListings);
+      });
+  }
+  
+  ngOnDestroy(): void {
+    if (this.queryParamsSubscription) {
+      this.queryParamsSubscription.unsubscribe();
+    }
+  }
 }
