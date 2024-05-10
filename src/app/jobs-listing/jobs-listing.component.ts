ccfd501b707656d0ef6a858ddc569bc20cf22c57
mithun30052001 <lmithun01@gmail.com>
2024-05-10 15:13:34 +0530

    job listing is rendered inside filter-consumer-container

--- src/app/jobs-listing/jobs-listing.component.ts
+++ src/app/jobs-listing/jobs-listing.component.ts
@@ -1,43 +1,30 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { Component, OnInit } from '@angular/core';
 import { JobsService } from '../services/job.services';
 import { GenericSelectionComponent } from '../models/generic-selection';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-jobs-listing',
   templateUrl: './jobs-listing.component.html',
   styleUrls: ['./jobs-listing.component.scss'],
 })
-export class JobsListingComponent implements OnInit, OnDestroy {
-  filteredJobListings: any[] = [];
-  pageSizeOptions: number[] = [5, 10, 25, 100];
-  itemsPerPage: number = 5;
-  private queryParamsSubscription!: Subscription;
+export class JobsListingComponent implements OnInit{
+  params: any;
+  jobs: any[] = [];
+  filteredJobs: any[] = [];
 
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
     private jobsService: JobsService,
     private genericSelection: GenericSelectionComponent
   ) {}
 
-  ngOnInit(): void {
-    this.queryParamsSubscription =
-      this.genericSelection.allQueryParams$.subscribe((params) => {
-        console.log('Inside job listing', params);
-        this.filteredJobListings = this.jobsService.getJobListings(params);
-        if ('itemsPerPage' in params) {
-          this.itemsPerPage = parseInt(params['itemsPerPage']as string);
-        }
-        this.filteredJobListings = this.filteredJobListings.slice(0,this.itemsPerPage);
-      });
+  ngOnInit() {
+    this.genericSelection.allQueryParams$.subscribe((params) => {
+      this.params = params;
+      this.jobs = this.jobsService.getJobListings(params);
+    });
   }
-  
-  ngOnDestroy(): void {
-    if (this.queryParamsSubscription) {
-      this.queryParamsSubscription.unsubscribe();
-    }
+
+  updateJobs(newListings: any[]): void {
+    this.filteredJobs= newListings;
   }
 }
