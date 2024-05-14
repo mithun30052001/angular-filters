45d384b01862fba951cbd1d99cac7843c71d991f
mithun30052001 <lmithun01@gmail.com>
2024-05-14 11:41:28 +0530

    unwanted output is removed

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,6 +1,5 @@
 
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { JobsService } from '../services/job.services';
+import { Component, Input } from '@angular/core';
 import { QueryParamsService } from '../models/query-params.service';
 import { PageEvent } from '@angular/material/paginator';
 
@@ -12,11 +11,10 @@ import { PageEvent } from '@angular/material/paginator';
 export class PaginationComponent {
   @Input() totalItems: number = 0;
   @Input() itemsPerPage: number = 10;
-  @Output() filteredJobListingsChange = new EventEmitter<any[]>();
   pageSizeOptions: number[] = [1, 5, 10, 25, 100];
   currentPageIndex: number = 0;
 
-  constructor(private jobsService: JobsService, private queryParams: QueryParamsService) {}
+  constructor(private queryParams: QueryParamsService) {}
 
   onPageChange(event: PageEvent) {
     this.currentPageIndex = event.pageIndex;
