ba863f0fd69649ad5b6ef3282480aee226c21acf
mithun30052001 <lmithun01@gmail.com>
2024-05-14 12:30:36 +0530

    All subscriptions are unsubscribed

--- src/app/jobs-listing/jobs-listing.component.ts
+++ src/app/jobs-listing/jobs-listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { JobsService } from '../services/job.services';
 import { QueryParamsService } from '../models/query-params.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-jobs-listing',
@@ -10,7 +11,8 @@ import { QueryParamsService } from '../models/query-params.service';
 export class JobsListingComponent implements OnInit{
   jobs: any[] = [];
   filteredJobs: any[] = [];
-
+  private queryParamsSubscription!: Subscription;
+  
   /**
    * @param jobsService - Service to fetch job listings.
    * @param queryParams - Service to manage query parameters.
@@ -22,11 +24,17 @@ export class JobsListingComponent implements OnInit{
   ) {}
 
   ngOnInit() {
-    this.queryParams.allQueryParams$.subscribe((params) => {
-      this.jobs = this.jobsService.getJobListings(params);
+    this.queryParamsSubscription = this.queryParams.allQueryParams$.subscribe((params) => {
+       this.jobs = this.jobsService.getJobListings(params);
     });
   }
-
+  
+  ngOnDestroy(): void {
+    if (this.queryParamsSubscription) {
+      this.queryParamsSubscription.unsubscribe();
+    }
+  }
+  
   /**
    * Method to update the filtered job listings.
    * @param newListings - Array of new job listings to update the filteredJobs array.
--- src/app/referrals/referrals.component.ts
+++ src/app/referrals/referrals.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { QueryParamsService } from 'src/app/models/query-params.service';
 import { JobsService } from 'src/app/services/job.services';
 
@@ -10,16 +11,23 @@ import { JobsService } from 'src/app/services/job.services';
 export class ReferralsComponent {
   referrals: any[] = [];
   filteredReferrals: any[] = [];
-
+  private queryParamsSubscription!: Subscription;
+  
   constructor(
     private queryParams: QueryParamsService,
     private jobsService: JobsService
   ) {}
 
   ngOnInit() {
-    this.queryParams.allQueryParams$.subscribe((params) => {
-      this.referrals = this.jobsService.getJobListings(params);
+    this.queryParamsSubscription = this.queryParams.allQueryParams$.subscribe((params) => {
+        this.referrals = this.jobsService.getJobListings(params);
     });
+  }
+  
+  ngOnDestroy(): void {
+    if (this.queryParamsSubscription) {
+      this.queryParamsSubscription.unsubscribe();
+    }
   }
 
   updateReferrals(newListings: any[]): void {
